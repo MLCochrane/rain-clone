e25dbfffd0926aa4e786a57aef20ad35d9b71dd4
Luke <lukecochrane2@gmail.com>
2018-09-11 19:50:27 -0400

    Updated app.js with barba views, page transitions

--- src/app.js
+++ src/app.js
@@ -1,155 +1,106 @@
 import './scss/app.scss';
 import $ from 'jquery';
+import Barba from 'barba.js';
+import {TweenMax, TimelineLite, Power2} from 'gsap';
 // import 'jquery-ui/ui/widget';
 // import 'jquery-ui/ui/widgets/draggable';
 
+/*
+* TEMPLATES
+*/
+import Player from './js/player.js';
+import Animate from './js/anime.js';
+import Slider from './js/home-slider.js';
 
-class Player {
-  constructor(url) {
-    this.url = url;
-    this.init();
-  }
 
-  init() {
-    this.getScrollAmt();
-    this.bindEvents();
-  }
-
-  getScrollAmt() {
-    this.maxHeight = window.innerWidth / (16/9);
-    if (window.innerWidth <= 755) {
-      this.origHeight = 321;
-    } else {
-      this.origHeight = 525;
+$(document).ready(function() {
+  let slider;
+  const HomeView = Barba.BaseView.extend({
+    namespace: 'home',
+    onEnter() {
+      console.log('HomeView onEnter');
+    },
+    onEnterCompleted() {
+      console.log('HomeView onEnterCompleted');
+      slider = new Slider();
+    },
+    onLeave() {
+      console.log('onLeave');
+      slider.destroy();
+    // The Container has just been removed from the DOM.
     }
-  }
+  });
 
-  bindEvents() {
-    $('.start').on('click', e => {
-      // Load video and hide button
-      this.loadVideo();
-      $(e.currentTarget).toggle();
-      $('.loading').toggle();
-    });
-
-    // Custom controls for video
-    $('.play').on('click', () => {
-      this.player.playVideo();
-    });
-    $('.pause').on('click', () => {
-      this.player.pauseVideo();
-    });
-
-    // Mouse move event for displaying buttons
-    // let moving;
-    $('.slider-container').on('mousemove', () => {
-      console.log('container')
-      // clearTimeout(moving);
-      // moving = setTimeout(() => {
-      //   console.log('count');
-      // }, 100);
-    });
-
-    $('.video-placeholder').on('mousemove', () => {
-      console.log('iframe');
-    });
-
-    // Reset height variables on resize
-    let resizeWindow;
-    $(window).on('resize', () => {
-      clearTimeout(resizeWindow);
-      resizeWindow = setTimeout(() => {
-        this.getScrollAmt();
-      }, 100);
-    });
-  }
-
-  loadVideo() {
-    this.addScript().then(e => {
-      const vidID = this.getId();
-      setTimeout(() => {
-        this.initYoutube(vidID);
-      }, 100);
-    }).catch(e => {
-      console.log(e);
-    })
-  }
-
-  addScript() {
-    return new Promise((resolve, reject) => {
-      let tag = document.createElement('script');
-      tag.type = 'text/javascript';
-      tag.src = "https://www.youtube.com/iframe_api";
-      $(tag).on('load', () => resolve(tag));
-      $(tag).on('error', () => reject(tag));
-      document.body.appendChild(tag);
-    });
-  }
-
-  // Get ID from share link
-  getId() {
-    let id = (this.url).split('/');
-    id = id[id.length - 1];
-    return id;
-  }
-
-  // Init the youtube player with ID
-  initYoutube(id) {
-    this.player = new YT.Player('video-placeholder', {
-      videoId: id,
-      playerVars: {
-        autoplay: 1,
-        controls: 0,
-      },
-      events: {
-        'onReady': this.onPlayerReady.bind(this),
-        'onStateChange': this.onStateChange.bind(this),
-      }
-    });
-  }
-
-  onPlayerReady() {
-    // When video player is ready, animate the player view
-    this.transitionPlayer('open', this.maxHeight);
-    $('.loading').toggle();
-  }
-
-  onStateChange(e) {
-    if (e.data === 1) {
-      // console.log('Playing');
-      $('.play').css('display', 'none');
-      $('.pause').css('display', 'inherit');
-    } else if (e.data === 2) {
-      // console.log('Paused');
-      $('.play').css('display', 'inherit');
-      $('.pause').css('display', 'none');
-    } else if (e.data === 0) {
-      // Close open view and destroy the player
-      this.transitionPlayer('close', this.origHeight);
-      $('button').toggle();
-      this.player.destroy();
+  const PlayerView = Barba.BaseView.extend({
+    namespace: 'player',
+    onEnter() {
+      console.log('PlayerView onEnter');
+    },
+    onEnterCompleted() {
+      console.log('PlayerView onEnterCompleted');
+      const video = new Player('https://youtu.be/nYFEf7Y1Gco');
     }
-  }
+  });
+  const ProjectView = Barba.BaseView.extend({
+    namespace: 'anime',
+    onEnter() {
+      console.log('Anime onEnter');
+    },
+    onEnterCompleted() {
+      const animations = new Animate();
+    }
+  });
 
-  // Animations for opening and closing the player
-  transitionPlayer(status, height) {
-    const el = $('.slider-container');
-    $(el).animate({
-      height: height,
-    }, {
-      duration: 500,
-      complete: () => {
-        if (status === 'open') {
-          $(el).css('height', 'auto');
-        } else {
-          $(el).removeAttr('style');
+  // Init views
+  HomeView.init();
+  PlayerView.init();
+  ProjectView.init();
+
+  // Transition
+  var HideShowTransition = Barba.BaseTransition.extend({
+    start() {
+      Promise
+        .all([this.newContainerLoading, this.animating()])
+        .then(this.finish.bind(this));
+    },
+
+    animating() {
+      var deferred = Barba.Utils.deferred();
+      var tl = new TimelineLite();
+      let el = $(this.oldContainer).find('.page-main');
+      let copy = $(el).find('.title');
+
+      tl
+      .set(el, {autoAlpha: 1}, 0)
+      .to(copy, 0.5, {left: '-200px'}, 0)
+      .to(el, 1, {autoAlpha: 0, onComplete: () => {
+          deferred.resolve();
+        }}, 0);
+
+      return deferred.promise;
+    },
+
+    finish() {
+      $(this.oldContainer).hide();
+      $(this.newContainer).css(
+        {
+          opacity: 0,
+          visibility: 'visible'
         }
-      }
-    });
-  }
-}
+      );
+      $(this.newContainer).animate({ opacity: 1 }, 400, () => {
+        this.done();
+      })
+    }
+  });
 
-const video = new Player('https://youtu.be/nYFEf7Y1Gco');
+  Barba.Pjax.getTransition = function() {
+    return HideShowTransition;
+  };
+
+  // Barba.Prefetch.init();
+  Barba.Pjax.start();
+});
 
 
 //////////////////////////////////
---
+++ src/js/anime.js
@@ -0,0 +1,13 @@
+import $ from 'jquery';
+import anime from 'animejs';
+
+export default class Animate {
+  constructor() {
+    this.init();
+  }
+
+  init() {
+    console.log('hi');
+    this.startAnimation();
+  }
+}
